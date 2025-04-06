7c23ea0d5ccc998ca4afad7ee38041df03cdbebb
CoffeGlory <aqiu131@outlook.com>
2025-04-05 21:46:06 -0700

    array problem 4 median of two sorted array

---
+++ A4_MedianOfTwoSortedArray.java
@@ -0,0 +1,70 @@
+public class A4_MedianOfTwoSortedArray {
+    //require for O(log(m+n))
+    public static void main(String[] args) {
+        
+        int[] nums1 = {1,3};
+        int[] nums2 = {2};
+        A4_MedianOfTwoSortedArray solution = new A4_MedianOfTwoSortedArray();
+        double result = solution.findMedianSortedArrays(nums1, nums2);
+        System.out.println("Median: " + result);
+        
+        // Test case 2
+        int[] nums1_2 = {1, 2}; 
+        int[] nums2_2 = {3, 4};
+        double result_2 = solution.findMedianSortedArrays(nums1_2, nums2_2);
+        System.out.println("Median: " + result_2);
+    }
+
+    //Leetcode offical solution
+    /* by finding median of both arrays
+    * that is, the max of both left part should not be greater than the min of both right part
+    * and by modify the median location to solve this problem
+     */ 
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2){
+        double result = 0.0;
+        if(nums1.length > nums2.length){
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int totalLeft = (m + n + 1) / 2;
+
+        //divide the array into two parts, left and right
+        //left part is nums1[0..i] and nums2[0..j]
+        int left = 0;
+        int right = m;
+
+        while(left < right){
+            int i = left + (right - left + 1) / 2;
+            int j = totalLeft - i;
+            if(nums1[i - 1] > nums2[j]){
+                right = i - 1;   
+            }else{
+                left = i;
+            }
+        }
+
+        int i = left;
+        int j = totalLeft - i;
+
+        //Ternary Operator that is used to check if the index is out of bound
+        int nums1LeftMax = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
+        int nums1RightMin = i == m ? Integer.MAX_VALUE : nums1[i];
+        int nums2LeftMax = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
+        int nums2RightMin = j == n ? Integer.MAX_VALUE : nums2[j];
+
+        if (((m+n)%2) ==1){
+            result = Math.max(nums1LeftMax, nums2LeftMax);
+            
+        } else {
+            result = (Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin)) / 2.0;
+        }
+
+        return result;
+    }
+}
